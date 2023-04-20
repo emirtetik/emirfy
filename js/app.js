f677036f9e1a4b950c16134097d8bb0030d61e63
Emir Tetik <info@frontmir.com>
2023-04-20 17:29:57 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+// jsdelivr
+
+const text =  baffle(".data");
+text.set({
+  characters: '从A到Z<排序',
+        speed: 100
+});
+
+text.start();
+text.reveal(4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const text = document.getElementById("text");
+//   const message = "Frontend Developer";
+//   let i = 0;
+  
+//   function typeWriter() {
+//     if (i < message.length) {
+//       text.innerHTML += message.charAt(i);
+//       i++;
+//       setTimeout(typeWriter, 50); // her 50 milisaniyede bir karakter ekle
+//     }
+//   }
+  
+//   typeWriter(); // yazıyı karakter karakter yazdırmak için typeWriter fonksiyonunu çağır
+  
+
+
+
+
+
+
